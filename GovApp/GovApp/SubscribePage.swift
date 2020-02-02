740f807e7aa231e0ca7ca096b5e5cb8503e63da3
cs233-mvelasque <madison@Madisons-MacBook-Pro.local>
2020-02-01 20:51:46 -0800

    Added table view

--- GovApp/GovApp/SubscribePage.swift
+++ GovApp/GovApp/SubscribePage.swift
@@ -10,21 +10,66 @@ import UIKit
 
 class SubscribePage: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    let subArray = ["Traffic", "Weather", "Police"]
+    var SearchArray = [String]()
+    var searching = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
         // Do any additional setup after loading the view.
+    
     }
+}
+
+
+
+
+extension SubscribePage: UITableViewDataSource, UITableViewDelegate{
+   
+    
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section : Int) -> Int
+    {
+        if searching{
+            return SearchArray.count
+        }
+        else{
+             return subArray.count
+        }
+   
+    }
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+            if searching{
+                cell?.textLabel?.text = SearchArray[indexPath.row]
+            }
+            else
+            {
+               cell?.textLabel?.text = subArray[indexPath.row]
+            }
+        
+        return cell!
+    }
+}
+extension SubscribePage: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchtext: String)
+    {
+        SearchArray = subArray.filter({$0.prefix(searchtext.count) == searchtext})
+        searching = true
+        tableView.reloadData()
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func searchBarCancelButton(_ searchBar:UISearchBar)
+    {
+        searching = false
+        searchBar.text = " "
+        tableView.reloadData()
     }
-    */
-
 }
+}
+
